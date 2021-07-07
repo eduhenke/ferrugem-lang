35d40f3b97025ec6a2687ace64ed5e185698ab4c
eduhenke <eduardo_henke@hotmail.com>
2021-07-06 23:15:00 -0300

    Add file loading and CLI capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod lexer;
 
 use lexer::lex;
+use std::fs;
+
+use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+struct Cli {
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
 
 fn main() {
-    let result = lex("def foo (int a) {}");
+    let args = Cli::from_args();
+    let data = fs::read_to_string(args.path.to_str().unwrap()).expect("Unable to read file");
+    let result = lex(data.as_str());
 
     for token in result {
         println!("{:?}", token);
